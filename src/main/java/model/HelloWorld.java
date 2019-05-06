887a77c8bb10454d7424db1eb8274293e50c1669
NicolasFlorent <nicolasf.mateus@gmail.com>
2019-05-06 15:04:59 +0200

    Modification

---
+++ src/main/java/model/HelloWorld.java
@@ -0,0 +1,12 @@
+package main.java.model;
+
+// import java.util;
+
+
+public class HelloWorld {
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/test/HelloWorldTest.java
@@ -0,0 +1,20 @@
+package test.java.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloWorldTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
